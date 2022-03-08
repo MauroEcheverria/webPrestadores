2d5fb95d9eef825d251ae3ad340aff78efab7917
DreConsTec <kaceto104@gmail.com>
2022-03-07 21:46:40 -0500

    Update prestadores_iess.sql

--- prestadores_iess.sql
+++ prestadores_iess.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 07-03-2022 a las 01:58:27
+-- Tiempo de generación: 08-03-2022 a las 03:31:44
 -- Versión del servidor: 10.4.10-MariaDB
 -- Versión de PHP: 7.3.12
 
