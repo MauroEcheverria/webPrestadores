3af92835d2778cec54300bdd916789c629255ce6
DreConsTec <kaceto104@gmail.com>
2022-07-30 10:16:36 -0500

    Update templateHead.php

--- template/templateHead.php
+++ template/templateHead.php
@@ -31,10 +31,7 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
               </li>
               <li class="nav-item d-none d-sm-inline-block">
-                <a href="index3.html" class="nav-link">Home</a>
-              </li>
-              <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">Contact</a>
+                <a class="nav-link"><strong>Ambiente: <?php echo $dataSesion["tipo_ambiente"] ?></strong></a>
               </li>
             </ul>
             <ul class="navbar-nav ml-auto">
